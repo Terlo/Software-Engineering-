7986a050f38b1f90e77b83b65e760cf840d60d81
Terlo <57776535+Terlo@users.noreply.github.com>
2021-10-11 17:56:54 +0100

    addition of the test file
    addition of the test files and headers for the test functions

---
+++ LowestCommonAncestorTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class LowestCommonAncestorTest {
+
+    // test lca when the root node is the common ancestor between the two nodes
+    @Test
+    public void testRootisLCA() {
+
+    }
+
+    // test lca function when there exists no LCA between the nodes
+    @Test
+    public void testNoLCA() {
+
+    }
+
+    // test lca function when the lca is one of the selected nodes
+    @Test
+    public void testNodeIsLca() {
+
+    }
+
+    // test lca when the binary tree is empty
+    @Test
+    public void testEmptyBinaryTree() {
+
+    }
+
+    // test lca when at least one of the nodes entered is not in the tree
+    @Test
+    public void test() {
+
+    }
+
+}
